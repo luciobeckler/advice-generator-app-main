da53bb6b59adae091be63249622bb57eecf13eb1
Lucio <luciobecklerp@gmail.com>
2022-09-27 17:15:43 -0300

    Faltando implementar o efeito no botão de busca

---
+++ src/main.js
@@ -0,0 +1,14 @@
+let adviceText = document.querySelector(".adviceText")
+let button = document.querySelector(".reloadButton")
+const url = "https://api.adviceslip.com/advice"
+
+button.addEventListener('click', function () {
+      fetch(url)
+            .then(response => { return response.json() })
+            .then(advice => {
+                  adviceText.innerHTML = "\"" + advice.slip.advice + "\""
+
+                  button.classList.add('ef-glow')
+                  button.classList.remove('ef-glow')
+            })
+})
