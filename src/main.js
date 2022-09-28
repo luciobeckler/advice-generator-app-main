b1374618d1047c5f0abfb28af02278acd85d40bb
Lucio <luciobecklerp@gmail.com>
2022-09-28 09:43:26 -0300

    Projeto concluído

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 let adviceText = document.querySelector(".adviceText")
 let button = document.querySelector(".reloadButton")
+let adviceID = document.querySelector(".adviceID")
 const url = "https://api.adviceslip.com/advice"
 
 button.addEventListener('click', function () {
@@ -7,7 +8,7 @@ button.addEventListener('click', function () {
             .then(response => { return response.json() })
             .then(advice => {
                   adviceText.innerHTML = "\"" + advice.slip.advice + "\""
-
+                  adviceID.innerHTML = advice.slip.id
             })
 
 
